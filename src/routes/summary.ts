b2b56d5dc62e7d5775d8a803892d7cef096e0013
Thiago Bignotto <bignotto@gmail.com>
2024-07-02 06:16:21 -0300

    :floppy_disk: save commit

--- src/routes/summary.ts
+++ src/routes/summary.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance } from "fastify";
-import knex from "knex";
+import { knex } from "../database";
 import { checkSessionIdExists } from "../middlewares/check-session-id-exists";
 
 export async function summaryRoutes(app: FastifyInstance) {
@@ -11,20 +11,31 @@ export async function summaryRoutes(app: FastifyInstance) {
     async (request, reply) => {
       const { sessionId } = request.cookies;
 
-      const meals = await knex("meals")
-        .where({
-          user_id: sessionId,
-        })
-        .select();
+      const meals = await knex("meals").where("user_id", sessionId).select();
 
       const mealsTotal = meals.length;
-      const onDietMealsTotal = meals.filter((m) => m.on_diet === true);
-      const offDietMealsTotal = meals.filter((m) => m.on_diet === false);
+      const onDietMealsTotal = meals.filter((m) => !!m.on_diet === true).length;
+      const offDietMealsTotal = meals.filter(
+        (m) => !!m.on_diet === false,
+      ).length;
+
+      let streak = 0;
+      let bestStreak = 0;
+      for (let i = 0; i < meals.length; i++) {
+        console.log({ i, streak, bestStreak });
+        if (!!meals[i].on_diet === false) {
+          bestStreak = streak > bestStreak ? streak : bestStreak;
+          streak = 0;
+          continue;
+        }
+        streak++;
+      }
 
       return {
         mealsTotal,
         onDietMealsTotal,
         offDietMealsTotal,
+        bestStreak: streak > bestStreak ? streak : bestStreak,
       };
     },
   );
